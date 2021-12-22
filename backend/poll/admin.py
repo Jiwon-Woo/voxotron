9e1159a80ce3c37c9fe9881302a9fa364248c5da
Jiwon Woo <jiwonwoo@MacBookAir.local>
2021-12-22 13:36:27 +0900

    backend: register pollInfo model to admin
    create ModelAdmin named CreatePollAdmin
    
    Co-authored-by: Jiwon-Woo <jwoo@student.42seoul.kr>
    Co-authored-by: yeshin Lee <yeslee@student.42seoul.kr>

--- backend/poll/admin.py
+++ backend/poll/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import PollInfo
 
-# Register your models here.
+
+@admin.register(PollInfo)
+class CreatePollAdmin(admin.ModelAdmin):
+    list_display = (
+        'poll_id',
+        'begin_at',
+        'end_at',
+        'nbr_voices',
+        'logins_voters',
+        'logins_cands',
+    )
+    search_fields = (
+        'poll_id',
+        'begin_at',
+        'end_at',
+        'nbr_voices',
+        'logins_voters',
+        'logins_cands',
+    )
