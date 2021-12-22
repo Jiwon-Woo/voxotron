c5ec8038bd2c26e479ef83ea77f16774e47b3255
Jiwon Woo <jiwonwoo@MacBookAir.local>
2021-12-22 13:32:28 +0900

    backend: make model named PollInfo
    make PollInfo according to database and do makemigrations
    
    Co-authored-by: Jiwon-Woo <jwoo@student.42seoul.kr>
    Co-authored-by: yeshin Lee <yeslee@student.42seoul.kr>

--- backend/poll/models.py
+++ backend/poll/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class PollInfo(models.Model):
+    poll_id = models.AutoField(primary_key=True)
+    begin_at = models.DateTimeField(null=False)
+    end_at = models.DateTimeField(null=False)
+    nbr_voices = models.PositiveIntegerField(null=True)
+    logins_voters = models.TextField(null=False)
+    logins_cands = models.TextField(null=False)
