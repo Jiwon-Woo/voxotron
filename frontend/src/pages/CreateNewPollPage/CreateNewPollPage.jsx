5fb7c7cc49ea4b93d067107ce0c05aa329778310
chichoon <chichoon.choi@gmail.com>
2021-12-23 13:30:35 +0900

    frontend: divide pages
    이전 작업물에서 컴포넌트 사용하여 페이지 세팅

---
+++ frontend/src/pages/CreateNewPollPage/CreateNewPollPage.jsx
@@ -0,0 +1,13 @@
+import { React } from 'react';
+import { CreateNewPollForm } from '../../components';
+
+const CreateNewPollPage = () => {
+  return (
+    <>
+      <h1>hi</h1>
+      <CreateNewPollForm />
+    </>
+  );
+};
+
+export default CreateNewPollPage;
---
+++ frontend/src/pages/PollListPage/PollListPage.jsx
@@ -0,0 +1,12 @@
+import { React } from 'react';
+import { ViewPollsContainer } from '../../components';
+
+const PollListPage = () => {
+  return (
+    <>
+      <ViewPollsContainer />
+    </>
+  );
+};
+
+export default PollListPage;
