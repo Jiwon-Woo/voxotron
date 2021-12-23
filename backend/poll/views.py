ab1edfcc0aced55dd54597bb5dac4840b6b6d7a9
Yeshin Lee <yeslee5004@gmail.com>
2021-12-23 17:09:41 +0900

    backend: modify value(logins_voters, logins_cands) to put DB
    Co-authored-by: jwoo <jwoo@student.42seoul.kr>
    Co-authored-by: yeslee <yeslee@student.42seoul.kr>

--- backend/poll/views.py
+++ backend/poll/views.py
@@ -3,6 +3,19 @@ from .models import PollInfo
 from rest_framework import generics, response, status
 
 
+def replaceValue(key, value):
+    pre_value = value.__getitem__(key)
+    value.__setitem__(key, pre_value.replace('\r\n', '#'))
+    return value
+
+
+def modifyValue(request):
+    copy_value = request.data.copy()
+    next_value = replaceValue("logins_voters", copy_value)
+    next_value = replaceValue("logins_cands", next_value)
+    return next_value
+
+
 class PollListApi(generics.ListCreateAPIView):
     queryset = PollInfo.objects.all()
     serializer_class = PollSerializer
@@ -15,7 +28,8 @@ class PollListApi(generics.ListCreateAPIView):
         return response.Response(serializer.data[0])
 
     def post(self, request):
-        serializer = PollSerializer(data=request.data)
+        value = modifyValue(request)
+        serializer = PollSerializer(data=value)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return response.Response(serializer.data, status=status.HTTP_201_CREATED)
