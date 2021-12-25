d77a28aa7858fee30dee6c390697ce00c9f574d0
chichoon <chichoon.choi@gmail.com>
2021-12-26 00:03:15 +0900

    frontend: add main page
    네비게이션바로 이동 위해 메인페이지 추가

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,11 +1,17 @@
 import { React } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CreateNewPollPage, PollListPage, PollUpdatePage } from './pages';
+import {
+  CreateNewPollPage,
+  PollListPage,
+  PollUpdatePage,
+  MainPage,
+} from './pages';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route exact path="/" element={<MainPage />} />
         <Route exact path="/create" element={<CreateNewPollPage />} />
         <Route exact path="/poll" element={<PollListPage />} />
         <Route path="/poll/:poll_id/update" element={<PollUpdatePage />} />
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,5 +1,6 @@
 import CreateNewPollPage from './CreateNewPollPage';
 import PollListPage from './PollListPage';
 import PollUpdatePage from './PollUpdatePage';
+import MainPage from './MainPage';
 
-export { CreateNewPollPage, PollListPage, PollUpdatePage };
+export { CreateNewPollPage, PollListPage, PollUpdatePage, MainPage };
