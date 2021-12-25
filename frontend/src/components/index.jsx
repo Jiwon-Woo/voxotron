9b7e0b8af2e9cbaa9cc2071012fa20fa68915bf4
chichoon <chichoon.choi@gmail.com>
2021-12-26 00:02:34 +0900

    frontend: add navigationBar
    페이지 이동을 위한 네비게이션 바 추가

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -1,5 +1,12 @@
 import ViewPollsContainer from './ViewPollsContainer';
 import UpdatePollForm from './UpdatePollForm';
 import { GetAllData, GetOneData } from './ServerDataComponent';
+import NavigationBar from './NavigationBar';
 
-export { ViewPollsContainer, UpdatePollForm, GetAllData, GetOneData };
+export {
+  ViewPollsContainer,
+  UpdatePollForm,
+  GetAllData,
+  GetOneData,
+  NavigationBar,
+};
