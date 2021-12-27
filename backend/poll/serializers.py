4ca41486d0a95a2f2b70510a0f382e38355fcec4
jiwonwoo <jwoo@student.42seoul.kr>
2021-12-27 10:13:16 +0900

    backend: modify api(GET)
    send multiple data with poll_id in list format
    
    Co-authored-by: Jiwon-Woo <jwoo@student.42seoul.kr>
    Co-authored-by: yeshin Lee <yeslee@student.42seoul.kr>

--- backend/poll/serializers.py
+++ backend/poll/serializers.py
@@ -6,6 +6,7 @@ class PollSerializer(serializers.ModelSerializer):
     class Meta:
         model = PollInfo
         fields = [
+            'poll_id',
             'begin_at',
             'end_at',
             'nbr_voices',
