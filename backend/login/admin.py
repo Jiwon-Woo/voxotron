2e3171c89fb31701db3925055c9a0c40e219d6a4
jiwonwoo <jwoo@student.42seoul.kr>
2022-01-02 15:57:35 +0900

    backend: add UserInfo model
    create UserInfo & register admin page
    
    Co-authored-by: Jiwon-Woo <jwoo@student.42seoul.kr>
    Co-authored-by: yeslee-v <yeslee5004@gmail.com>

--- backend/login/admin.py
+++ backend/login/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import UserInfo
 
-# Register your models here.
+
+@admin.register(UserInfo)
+class LoginUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'login_id',
+        'is_staff',
+        'group',
+        'anonymize_date',
+    )
+    search_fields = (
+        'id',
+        'login_id',
+        'group',
+    )
--- backend/login/models.py
+++ backend/login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    login_id = models.TextField(null=False)
+    is_staff = models.BooleanField(null=False)
+    group = models.TextField(null=False)
+    anonymize_date = models.DateTimeField(null=False)
