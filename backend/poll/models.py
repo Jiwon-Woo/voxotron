40d5e399fa97236408ab411066e3b509fabc434d
Yeshin Lee <yeslee5004@gmail.com>
2022-01-06 00:00:38 +0900

    backend: Modify logins_voters & logins_cands
    1. Import UserInfo from logins.models
    2. Modify field -> Foriegn key
    
    Co-authored-by: Jiwon-Woo <jwoo@student.42seoul.kr>
    Co-authored-by: yeslee-v <yeslee5004@gmail.com>

--- backend/poll/models.py
+++ backend/poll/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from login.models import UserInfo
 
 
 class PollInfo(models.Model):
@@ -6,5 +7,13 @@ class PollInfo(models.Model):
     begin_at = models.DateTimeField(null=False)
     end_at = models.DateTimeField(null=False)
     nbr_voices = models.PositiveIntegerField(null=True)
-    logins_voters = models.TextField(null=False)
-    logins_cands = models.TextField(null=False)
+    logins_voters = models.ForeignKey(
+        UserInfo,
+        on_delete=models.CASCADE,
+        related_name='logins_voters'
+    )
+    logins_cands = models.ForeignKey(
+        UserInfo,
+        on_delete=models.CASCADE,
+        related_name='logins_cands'
+    )
